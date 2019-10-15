e73f0d61332b7162b8239abdcf6f38332530d293
Dimple <dimplejbhar@gmail.com>
2019-10-15 16:23:38 +0200

    changes

--- src/js/index.js
+++ src/js/index.js
@@ -3,10 +3,17 @@ const songs = [
     'https://scummbar.com/mi2/MI1-CD/02%20-%20Chapter%20Screen.mp3',
     'https://scummbar.com/mi2/MI1-CD/03%20-%20The%20Scumm%20Bar.mp3'
 ];
+const divText = document.querySelector('.text');
+const barfill = document.querySelector(".barfill")
+
 let currentSong = 0;
 let audio = new Audio(songs[currentSong]); // Descarga la primera canción
 audio.volume = 0.5;
-const divText = document.querySelector('.text');
+
+// const divText = document.querySelector('.text');
+// const barfill = document.querySelector(".barfill");
+// const bartime = document.querySelector(".bartime");
+
 const buttons = {
     play: document.querySelector('.play'),
     pause: document.querySelector('.pause'),
@@ -20,12 +27,14 @@ function fnPlay() {
     prepareSong();
     audio.play();
     putText();
+
 }
 
 function prepareSong() {
     fnPause();
     audio = new Audio(songs[currentSong]);
     audio.volume = 0.5;
+
 }
 
 function fnPause() {
@@ -34,92 +43,46 @@ function fnPause() {
 
 function fnPrev() {
     currentSong--;
+
     if (currentSong < 0) {
         currentSong = songs.length - 1;
     }
+
     fnPlay();
 }
 
 function fnNext() {
     currentSong++;
+ 
     if (currentSong >= songs.length) {
         currentSong = 0;
     }
+ 
     fnPlay();
 }
-
+ 
 function fnStop() {
     audio.currentTime = 0;
     fnPause();
 }
 
+
+ 
 function changeVolume(vol, name) {
     audio.volume = vol;
     buttons.volume.innerHTML = `<i class="fas fa-volume-${name}"></i>`;
 }
-
+ 
 function fnVolume() {
-
-    // Way 1 (ERROR!!)
-    // if (audio.volume == 0.5) {
-    //     audio.volume = 1;
-    //     buttons.volume.innerHTML = '<i class="fas fa-volume-up"></i>';
-    // }
-    // if (audio.volume == 1) {
-    //     audio.volume = 0;
-    //     buttons.volume.innerHTML = '<i class="fas fa-volume-mute"></i>';
-    // }
-    // if (audio.volume == 0) {
-    //     audio.volume = 0.5;
-    //     buttons.volume.innerHTML = '<i class="fas fa-volume-down"></i>';
-    // }
-
-    // Way 2 (ELSE!)
-    // if (audio.volume == 0.5) {
-    //     audio.volume = 1;
-    //     buttons.volume.innerHTML = '<i class="fas fa-volume-up"></i>';
-    // } else if (audio.volume == 1) {
-    //     audio.volume = 0;
-    //     buttons.volume.innerHTML = '<i class="fas fa-volume-mute"></i>';
-    // } else if (audio.volume == 0) {
-    //     audio.volume = 0.5;
-    //     buttons.volume.innerHTML = '<i class="fas fa-volume-down"></i>';
-    // }
-
-    // Way 3 (Refactor)
-    // En nuestro caso, se puede eliminar el último if
+ 
     if (audio.volume == 0.5)
         changeVolume(1, 'up');
     else if (audio.volume == 1)
         changeVolume(0, 'mute');
-    else if (audio.volume == 0)
+    else 
         changeVolume(0.5, 'down');
-
-    // Way 4 (Switch)
-    // switch (audio.volume) {
-    //     case 0.5:
-    //         changeVolume(1, 'up');
-    //         break;
-    //     case 1:
-    //         changeVolume(0, 'mute');
-    //         break;
-    //     case 0:
-    //         changeVolume(0.5, 'down');
-    //         break;
-    //     default:
-    //         alert('ERROR: Esto no puede ocurrir');
-    //         break;
-    // }
-
-    // Way 5 (CRAZY JAVASCRIPT ULTIMATE REFACTOR)
-    // const cases = {
-    //     0.5: changeVolume.bind(null, 1, 'up'),
-    //     1: changeVolume.bind(null, 0, 'mute'),
-    //     0: changeVolume.bind(null, 0.5, 'down')
-    // }
-    cases[audio.volume]();
 }
-
+ 
 function putText() {
     const nameSong = songs[currentSong];
     const cleanName = unescape(nameSong);
@@ -128,6 +91,28 @@ function putText() {
     const miniName = shortName.replace('.mp3', '');
     divText.innerHTML = miniName;
 }
+
+// function timeUpdate () {
+//     barfill.style.width = ((audio.currentTime/audio.duration) * 100) + "%";
+// }
+// for (i = 0; i<audio.length)
+// bartime.textContent = audio.currentTime;
+
+
+
+
+// audio.addEventListener("timeupdate", progressBar);
+
+
+function progressBar() {
+    // barfill.style.width = ((audio.currentTime/audio.duration) * 100) + "%";
+    let porcentaje = (audio.currentTime/audio.duration) * 100
+    barfill.style.width = porcentaje + '%';
+}
+
+audio.addEventListener("timeupdate", progressBar());
+// audio.ontimeupdate = progressBar();
+
 buttons.play.addEventListener('click', fnPlay);
 buttons.pause.addEventListener('click', fnPause);
 buttons.prev.addEventListener('click', fnPrev);
